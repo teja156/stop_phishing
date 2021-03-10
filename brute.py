085e67e3b0dc4bf762629092cb6ee651c987e393
Teja Swaroop <42813446+teja156@users.noreply.github.com>
2021-03-10 15:39:12 +0530

    Update brute.py
    change password size.

--- brute.py
+++ brute.py
@@ -43,7 +43,7 @@ def submitForm(ip):
 	uname_size = random.randint(5,10)
 	uname = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k = uname_size))
 	passwd_size = random.randint(8,20)
-	passwd = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k = uname_size))
+	passwd = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k = passwd_size))
 	data = {'password':passwd}
 
 	if ip_port == "0":
